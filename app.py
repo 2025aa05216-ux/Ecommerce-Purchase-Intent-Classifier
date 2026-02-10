bbd70d39eaff9d5f15fc844a83b553f81d85a7fe
Soumya Jha <2025aa05216@wilp.bits-pilani.ac.in>
2026-02-10 10:16:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-#from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 
